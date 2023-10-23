2d0bd7fef657b41397c797beb0d69015d84f5a48
Quianey <qiw027@ucsd.edu>
2023-10-23 14:48:00 -0700

    fix multiple lowest can't be added up

--- ArrayTests.java
+++ ArrayTests.java
@@ -27,4 +27,35 @@ public class ArrayTests {
     int[] input1 = { 4, 4, 4, 4 };
     assertArrayEquals(new int[]{ 4, 4, 4, 4 }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testAverageZeroElement() {
+    double[] input1 = { };
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input1), 0.0001);
+  }
+  
+  @Test
+  public void testAverageOneElement() {
+    double[] input1 = { 1 };
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input1), 0.0001);
+  }
+
+  @Test
+  public void testAverageTwoElement() {
+    double[] input1 = { 1, 2 };
+    assertEquals(2, ArrayExamples.averageWithoutLowest(input1), 0.0001);
+  }
+
+  @Test
+  public void testAverageMultipleElement() {
+    double[] input1 = { 2, 7, 8, 8, 5, 2, 6, 7, 9};
+    assertEquals((7+8+8+5+2+6+7+9)/8.0, ArrayExamples.averageWithoutLowest(input1), 0.0001);
+  }
+
+  @Test
+  public void testAverageFourElement() {
+    double[] input1 = { 8, 8, 5, 9};
+    assertEquals((8+8+9)/3.0, ArrayExamples.averageWithoutLowest(input1), 0.0001);
+  }
+
 }
